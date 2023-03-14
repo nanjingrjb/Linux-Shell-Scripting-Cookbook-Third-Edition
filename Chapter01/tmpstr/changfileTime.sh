ddc6998193ea8762fc8ca90aa164902a36cd7ee6
nanjingrjb <nanjingrjb@gmail.com>
2023-03-14 22:17:23 +0800

    修改原名称为_hello

--- Chapter01/tmpstr/changfileTime.sh
+++ Chapter01/tmpstr/changfileTime.sh
@@ -2,8 +2,10 @@
 for i in `ls`;
  	#do tmp=`date +%Y-%m-%d-%H-%M-%s`;
 	#采用grep进行正则表达式的匹配
-	do echo grep "2023-03-14*" $i
-	#new=${i//$rep/_hello};
+	#do echo grep "2023-03-14*" $i
+	#do new=${i//_2023-03-14-21-49-1678801791/_hello};
 	#mv $i $new; 
+	do new=${i//_2023-03-14-21-49-1678801791/_hello};
+	mv $i $new; 
 done
 
