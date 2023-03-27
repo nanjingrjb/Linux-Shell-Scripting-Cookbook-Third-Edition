842ae4960f0663b7dae18895b141c70c2c56c11a
root <root@localhost.localdomain>
2023-03-28 07:38:54 +0800

    对于test和[]，其内部的==左右两边应空格，不然解析错误。

--- Chapter04/testjudge.sh
+++ Chapter04/testjudge.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 #测试字符a和b是否相等。
 test a == b;
-echo $?
+echo judge res is:$?
+
+echo "using [] judge"
+[ a == b ] && echo equal || echo not equal
+if  [ a == b ];
+then echo equal
+else
+	echo no equal
+fi
