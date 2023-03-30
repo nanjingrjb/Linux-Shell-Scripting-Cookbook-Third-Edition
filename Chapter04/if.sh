198fa9bcf5d99f14324a296a550668ca99cfff85
root <root@localhost.localdomain>
2023-03-30 08:55:41 +0800

    增加(())进行比较的操作，该操作支持>=这种c语言的写法。

--- Chapter04/if.sh
+++ Chapter04/if.sh
@@ -20,3 +20,16 @@ elif [ $score -gt 80 ]
 then echo "good"
 else echo "normal"
 fi
+
+
+echo "采用(())进行判断，只支持整数判断"
+
+if (($score < 60))
+then echo "failed"
+elif (($score >= 90))
+then echo "best"
+elif (($score >= 80 ))
+then echo "good"
+else echo "normal"
+fi
+
