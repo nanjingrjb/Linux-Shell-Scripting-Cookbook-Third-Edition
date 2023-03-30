05dbb9354378d883199c870b40443a61c8a59848
root <root@localhost.localdomain>
2023-03-30 08:47:28 +0800

    if的测试语句就是采用[]来判断，并且数据比较大小时，需要采用字符lt表示小于，gt表示大于，不支持>=这样写法。 本例可以用于后续扩展，比如一个自动打分系统，根据输入的一个文本文件，来自动打分。

---
+++ Chapter04/if.sh
@@ -0,0 +1,22 @@
+if date
+then 
+	echo "it worked"
+fi
+echo -e '\n'
+if asd
+then 
+	echo "it not worked"
+fi
+echo 'We are outside the if statement'
+
+
+read -p "your score:"  score
+echo $score
+if [ $score -lt 60 ]
+then echo "failed"
+elif [ $score -gt 90 ]
+then echo "best"
+elif [ $score -gt 80 ]
+then echo "good"
+else echo "normal"
+fi
