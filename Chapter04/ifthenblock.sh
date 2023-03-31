6a9386b37ee7ac0e52a17f9a84b6364a8325ed3a
nanjingrjb <nanjingrjb@gmail.com>
2023-03-31 15:47:58 +0800

    采用正则表达式进行文件匹配，寻找隐藏文件.可以采用 ls -al /home/test | grep "\.bash*"

--- Chapter04/ifthenblock.sh
+++ Chapter04/ifthenblock.sh
@@ -2,7 +2,7 @@
 
 # testing multiple commands in the then block
 #
-testuser=test1
+testuser=test
 #
 #if grep $testuser /etc/passwd
 if [ `grep $testuser /etc/passwd` ]
@@ -10,6 +10,8 @@ then
      echo "This is my first command in the then block."
      echo "This is my second command in the then block."
      echo "I can even put in other commands besides echo:"
+     echo "列出用户$testuser主目录下的bash文件如下:"
+     ls -al /home/$testuser| grep "\.bash*"
 fi
 echo "We are outside the if statement"
 
