68316199fede6aad0dbacbab6e0b8909f1fe24c9
root <root@localhost.localdomain>
2023-04-03 23:13:32 +0800

    对于采用重复连续数据的判断，其正则表达式需要采 "o\{3,5\}",即在连续字符后面加上反斜杠，相当于转义大括号。

--- Chapter04/vbird.sh
+++ Chapter04/vbird.sh
@@ -9,3 +9,34 @@ echo "寻找不含有the的行号,采用如下写法:grep -vn \"the\" regular_ex
 grep -vn "the" regular_express.txt
 
 
+echo "寻找忽略大小写的含有the的行号,采用如下写法:grep -in \"the\" regular_express.txt"
+grep -in "the" regular_express.txt
+
+
+echo "寻找含有test或tast的行号,采用如下写法:grep -n \"t[ae]st\" regular_express.txt"
+grep -n "t[ae]st" --color=auto regular_express.txt
+
+
+echo "寻找含有oo的行号,采用如下写法:grep -n \"oo\" regular_express.txt"
+grep -n "oo" regular_express.txt
+
+echo "寻找含有oo,但排除g开头的行号,采用如下写法:grep -n \"[^gf]oo\" regular_express.txt"
+grep -n "[^fg]oo" regular_express.txt --color=auto
+
+echo "寻找含有oo,但排除g开头的行号,采用如下写法:grep -n \"[^gf]oo\" regular_express.txt"
+grep -n "[^f]o{2}" regular_express.txt --color=auto
+
+echo "grep -n '[^a-z]oo' regular_express.txt"
+grep --color=auto -n '[^a-z]oo' regular_express.txt
+
+grep -n '[^[:lower:]]oo' regular_express.txt
+	
+echo "grep --color=auto -n '[0-9]' regular_express.txt"
+grep --color=auto -n '[0-9]' regular_express.txt
+
+grep --color=auto -n "o\{3,5\}" regular_express.txt
+
+
+
+
+
