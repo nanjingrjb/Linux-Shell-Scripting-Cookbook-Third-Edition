4b6b522f2bc114010791f3ab1ffa0f8bcc6ccd8a
nanjingrjb <nanjingrjb@gmail.com>
2023-04-07 20:19:12 +0800

    增加公司电脑的.bshrc，用于与家里面的同步

--- Chapter04/input.bat
+++ Chapter04/input.bat
@@ -1,27 +1,19 @@
 @echo off
-echo “请输入你的选择:”
-echo "开始请输入1"
-echo “继续请输入2”
-echo “退出请输入q”
-set /p choice=
-echo "你输入的是" %choice%
+
+for /l %%a in (1,1,3) do (
+echo %%a judge
+set /p xz=
+echo %xz%
+if %xz%==1 echo "ok"
+if %xz%==2 echo "bad"
+if %xz%==3 echo "cancel"
+
+)
+pause
 
 
-	if "%choice%"=="1" goto START
-	if "%choice%"=="2"  goto CONTINUE
-	if "%choice%"=="q"  goto QUIT
-	:START
-		echo "start your work"
-		set /p wait= 设置成功，按任意键退出
-		exit
-	:CONTINUE
-		echo "GO ON"
-		set /p wait= 设置成功，按任意键退出
-		exit
-	:QUIT
-		echo "BYE BYE"
-		set /p wait= 设置成功，按任意键退出
-		exit
+
+
 	
 
 	
