fed09fdcbe4ab35147e0ff17f0880854ee1821f2
nanjingrjb <nanjingrjb@gmail.com>
2023-04-07 15:02:31 +0800

    批处理中if并没有括号，而是直接跟在后面，不然if不管输入的是什么，一直执行都是输入1的情况

--- Chapter04/input.bat
+++ Chapter04/input.bat
@@ -4,12 +4,12 @@ echo "开始请输入1"
 echo “继续请输入2”
 echo “退出请输入q”
 set /p choice=
-echo "你输入的是"%choice%
+echo "你输入的是" %choice%
 
 
-	if("%choice%"=="1") goto START
-	if("%choice%"=="2") goto CONTINUE
-	if("%choice%"=="q") goto QUIT
+	if "%choice%"=="1" goto START
+	if "%choice%"=="2"  goto CONTINUE
+	if "%choice%"=="q"  goto QUIT
 	:START
 		echo "start your work"
 		set /p wait= 设置成功，按任意键退出
