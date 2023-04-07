0530c86f447c4a14199ded747cf559464d0456ac
nanjingrjb <nanjingrjb@gmail.com>
2023-04-07 15:29:26 +0800

    bat文件中的注释，中间注释用%remark%来表示，对于单行起头的注释，可以采用::
    :label表示程序的起始，一般用于goto 配合
    
    set /p choice=
    if "%choice%"=="1" goto start
    
    :start
      echo "start"
      set /p wait= 成功
      exit

---
+++ Chapter04/label_loop.bat
@@ -0,0 +1,9 @@
+      :LABEL             %打个标签%
+     REM  上面就是名为LABEL的标号                        %注释上面这句话的意思%
+     DIR C:\            %显示C盘根目录下的文件和文件夹%
+     DIR D:\            %显示D盘根目录下的文件和文件夹%
+     tree C:            %显示C盘的目录结构%
+     tree D:            %显示D盘的目录结构%
+	 timeout /t 10
+     GOTO LABEL         %返回到LABEL处%
+     REM  GOTO语句表示程序跳转LABEL处继续执行            %注释一下%
