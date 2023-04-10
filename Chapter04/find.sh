717971e30d5e1c9ad88a1fa6f2dea3d736174674
root <root@localhost.localdomain>
2023-04-11 06:28:10 +0800

    1)增加类型属性测试，-type f普通文件 d目录 b块文件 c字符文件 l链接文件 2)增加大小属性搜索，-size +1M超过1M -1M小于1M 3)名称正则检索，可以加引号，或转义字符\，不然报错。

--- Chapter04/find.sh
+++ Chapter04/find.sh
@@ -1,3 +1,14 @@
 find ./testfind -name file_[ab] -print 
 find ./testfind -name file_[ab] -delete	 
-find ./testfind -name file_[ab] |wc	 
+find ./testfind -name file_[ab] |wc	
+
+#采用find的条件 -type寻找
+echo "寻找文件夹 find ./testfind -type d |wc"
+find ./testfind -type d |wc
+
+
+echo "寻找文件 find ./testfind -type f |wc"
+ find ./testfind -type f |wc
+
+ echo "寻找txt文件，大小小于100m以内，find ~ -name \*.txt -type f -size -100M"
+ find ~ -name \*.txt -type f -size -100M |wc -l
