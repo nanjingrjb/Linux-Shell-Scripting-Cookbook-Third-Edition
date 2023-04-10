214987cc373d2cf11107290f699071cfb0406c95
root <root@localhost.localdomain>
2023-04-10 23:03:46 +0800

    if语句中非是在[!]，如文件夹非非存在时， if [ ! -d testfind ],原来在find路径不对，已经在 testfind文件夹下，所以用当前路径./
    find ./ -name file_[ab]|wc

--- Chapter04/mkdir.sh
+++ Chapter04/mkdir.sh
@@ -1,11 +1,17 @@
 #/bin/bash
 
 #创建100个文件夹，并创建26个文件
+if  [ ! -d testfind ];then
+   mkdir testfind
+else
+   rm -fr testfind
+   mkdir testfind
+fi
 
-mkdir ./testfind
 cd testfind
 mkdir dir_{0..100}
 touch dir_{0..100}/file_{a..z}
 
+pwd
 #寻找统计file*的个数
-find ./testfind -name file_a |wc
+find ./ -name file_[ab] |wc
