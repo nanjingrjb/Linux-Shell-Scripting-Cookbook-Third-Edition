4dbdaa1775dec44ef4e38703c81b547584bfd1ee
nanjingrjb <nanjingrjb@gmail.com>
2023-04-14 11:11:07 +0800

    增加对于bin文件中grep命令的查找，其中小括号用于多个选择，如'(linux|lsb)' 表示含有linux或lsb字符的命令。
    注意.+表示至少含有一个字符，排除某些字符写法为[^_-],排除连接符号。

---
+++ Chapter04/testgrep.sh
@@ -0,0 +1,19 @@
+#########################################################################
+# File Name: testgrep.sh
+# Author: ma6174
+# mail: ma6174@163.com
+# Created Time: Fri Apr 14 10:48:02 2023
+#########################################################################
+#!/bin/bash
+
+
+#优先采用扩展正则表达式进行文件内容搜索
+echo "搜索含有ls、sa开头的命令:grep  -n -E '^(ls|sa).+' ./testgrep/*.txt"
+grep --color=auto  -n -E '^(ls|sa).+' ./testgrep/*.txt
+
+echo "排除启动含有-_的命令,搜索含有ls、sa开头的命令:grep  -n -E '^(ls|sa).+' ./testgrep/*.txt"
+grep --color=auto  -n -E '^(ls|sa).[^/-/_].?' ./testgrep/*.txt
+
+echo "含有linux或lsb字符，中间含有连接符号_-的命令:grep  -n -E '[-_]+' ./testgrep/*.txt"
+grep --color=auto  -n -iE '(linux|lsb)[-_]+' ./testgrep/*.txt
+
