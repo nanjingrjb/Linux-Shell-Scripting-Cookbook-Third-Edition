9da9aca9d3555ef36f4802c46f030b195f401464
nanjingrjb <nanjing@gmail.com>
2023-04-17 08:03:08 +0800

    []内的变量之间需严格空格隔开，不然判非空，-n,-z都可以判字符为空。

--- Chapter04/teststrequ.sh
+++ Chapter04/teststrequ.sh
@@ -5,13 +5,38 @@
 # Created Time: Mon Apr 17 07:43:06 2023
 #########################################################################
 #!/bin/bash
-if [ $1='rich' ];then
+if [ $1 = "rich" ];then
 	echo "$1 equal."
 else
 	echo "$1"
 fi
-if [ !$2='poor' ];then
+if [ ! $2 = 'poor' ];then
 	echo "$2 not 'poor'"
 else
 	echo "$2"
 fi
+
+string1=Soccer
+string2=''
+#
+if [ -n $string1 ]
+then
+     echo "The string '$string1' is NOT empty"
+else
+     echo "The string '$string1' IS empty"
+fi
+#
+if [ -z $string2 ]
+then
+     echo "The string '$string2' IS empty"
+else
+     echo "The string '$string2' is NOT empty"
+fi
+#
+if [ -z $string3 ]
+then
+     echo "The string '$string3' IS empty"
+else
+     echo "The string '$string3' is NOT empty"
+fi
+
