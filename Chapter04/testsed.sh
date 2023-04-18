5880d240670b8d609b2d12dffe2f43b9b9823ff5
nanjingrjb <nanjingrjb@gmail.com>
2023-04-18 14:40:40 +0800

    测试sed的命令，实际需要再针对教程进行练习。

--- Chapter04/testsed.sh
+++ Chapter04/testsed.sh
@@ -9,3 +9,8 @@ echo "This is a test."|sed 's/test/big test/'
 
 #替换某个文件中的某些字段
 sed  's/python/c++/' ./newplace*.txt
+
+#采用扩展正则表达式来替换sed.txt的数字
+cat sed.txt
+echo "开始替换里面的数字"
+sed -E 's/[0-9]+/digit' sed.txt
