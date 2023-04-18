368934c5e5751c19a16ff9923f95c9b077bfbfec
nanjingrjb <nanjingrjb@gmail.com>
2023-04-18 21:38:53 +0800

    gawk的原理为：在命令序列中，以;来分割。 gawk '{BEGIN  print "the content of data3:";print $0 }' data3.txt

---
+++ shellbible/Chapter19-Scripts/gawk.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: gawk.sh
+# Author: ma6174
+# mail: ma6174@163.com
+# Created Time: Tue Apr 18 21:36:50 2023
+#########################################################################
+#!/bin/bash
+
+gawk 'BEGIN {print "the content of data3 :"};{print $0}' data3.txt
+
