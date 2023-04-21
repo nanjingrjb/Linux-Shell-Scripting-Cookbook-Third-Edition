49713043e8843662cc3cde70734a8b14c52a1026
nanjingrjb <nanjing@gmail.com>
2023-04-21 18:15:04 +0800

    编写sed脚本时，需要使用\插入和后接，且尾巴上以转义\'结束 其中a表示在尾巴插入，i表示在行前插入。 echo "give me your book."|sed 'a\thank you'

---
+++ shellbible/Chapter21-Scripts/sedinsert-add.sh
@@ -0,0 +1,9 @@
+#########################################################################
+# File Name: sedinsert-add.sh
+# Author: ma6174
+# mail: ma6174@163.com
+# Created Time: Fri Apr 21 18:12:34 2023
+#########################################################################
+#!/bin/bash
+echo "are you ok?"|sed 'a\iamok\'
+echo "are you ok?"|sed 'i\iamok\'
