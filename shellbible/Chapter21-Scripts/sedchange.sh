004a5db68df9eb62ef3754a5b379edbeabe1642a
nanjingrjb <nanjingrjb@gmail.com>
2023-04-23 10:06:56 +0800

    在文件的末尾插入一行，应该使用sed -i '$a\this is an end I insert.'new.txt 不加的话，sed会缺省在每行添加这一句话。

--- shellbible/Chapter21-Scripts/sedchange.sh
+++ shellbible/Chapter21-Scripts/sedchange.sh
@@ -14,9 +14,15 @@ sed 'y/Line/line/' data1.txt
 echo 'replace all '
 sed 's/Line/lines/' data1.txt
 echo 'write data to file:'
+if [ -f new.txt ];then 
+	rm new.txt
+fi
+
 sed '1,2w new.txt' data1.txt
 echo "new file content:"
 cat new.txt
 echo "read fact.sh and insert 3line of new.txt"
 sed -i '1r fact.sh' new.txt
+echo "I will add a end in new.txt"
+sed -i '$a\this is a end i add.' new.txt
 cat new.txt
