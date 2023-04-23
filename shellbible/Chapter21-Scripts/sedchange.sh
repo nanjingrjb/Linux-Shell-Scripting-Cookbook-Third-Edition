1d56718171b5da1ab3b93b15673f1a7a81ab74b5
nanjingrjb <nanjing@gmail.com>
2023-04-23 08:32:14 +0800

    sed '[range]w filename' srcfilename用于指定源文件的某些范围，写入到新文件中。

--- shellbible/Chapter21-Scripts/sedchange.sh
+++ shellbible/Chapter21-Scripts/sedchange.sh
@@ -10,6 +10,10 @@ cat data1.txt
 echo 'change 3:'
 sed '3c\this is a change for line3.' data1.txt
 echo 'y map 1to1'
-sed 'y/Line/lines/' data1.txt
+sed 'y/Line/line/' data1.txt
 echo 'replace all '
 sed 's/Line/lines/' data1.txt
+echo 'write data to file:'
+sed '1,2w new.txt' data1.txt
+echo "new file content:"
+cat new.txt
