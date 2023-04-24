98e2086f2d695b2be0420d69deea6365614d015a
nanjingrjb <nanjingrjb@gmail.com>
2023-04-24 14:46:32 +0800

    验证了对于备份文件中含有子文件夹时，需要将配置文件中的子文件夹信息提取出来， 由于linux文件夹只能逐级建，所以首先要建第一级子文件夹，然后将文件拷贝进去。
    主要采用了以下知识点：
    1）数组的动态生成。采用toke=(`echo $name|tr '/' ' '`)
    2)数组的访问 ${toke[0]},数组长度的判断${#toke[*]}
    3）if条件非判断，如文件夹不存在 if [ ! -d $dirnow ]
    4)打包文件的命令 tar -czvf desname.tar srcdir
    5)在linux中文件夹的拷贝是连通隐藏文件一起的，而windows拷贝是不拷贝隐藏文件。

--- Chapter04/tarspefile.sh
+++ Chapter04/tarspefile.sh
@@ -8,15 +8,37 @@
 config=ls.txt
 despath=/mnt/d/001_BaseVerBackup
 datenow=`date +%Y%m%d_%H%M%S`
+desdir=$despath/${datenow}_backups
 desname=${datenow}_backups.tar
+mkdir $desdir
 exec 0< $config
-read filename
-#echo $filename
+read filename outname
 while [ $? -eq 0 ]
 do
-	filelst="$filelst $filename"
-	read filename
+	#filelst="$filelst $filename"
+	#echo "in=$filename"
+	#echo "out=$outname"
+	if [ -e $filename ] && [ ! -z $outname ];then
+		#echo "all exist.please copy."
+		#if [ ! -e $desdir/$outname ];then
+		#	mkdir	$desdir/$outname
+		#fi
+		#echo $outname
+		toke=(`echo $outname|tr '/' ' '`)
+		if [ ${#toke[*]} -gt 1 ] && [ ! -d $desdir/${toke[0]} ];then
+			mkdir $desdir/${toke[0]}
+		fi
+		cp -r $filename $desdir/$outname
+	fi
+	read filename  outname
 done
+cd $despath
+tardir=`ls -lt  | grep $USER | head -n 1 |awk '{print $9}'`
+echo $tardir
+if [ -d $tardir/src_35L/.git  ];then
+	rm -fr $tardir/src_35L/.git
+fi
 
-#echo $filelst
-tar -czf $despath/$desname $filelst 2> /dev/null
+tar -cjf ${tardir}.tar  $tardir 2> /dev/null
+rm -fr $tardir
+echo "tar finished."
