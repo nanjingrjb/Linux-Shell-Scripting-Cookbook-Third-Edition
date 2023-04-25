a01134fadc3052c6d3365ec7236a5d046ed8fec8
nanjingrjb <nanjing@gmail.com>
2023-04-25 12:04:46 +0800

    gawk必须要打印全，才能显示所有值?

--- Chapter04/token.sh
+++ Chapter04/token.sh
@@ -5,11 +5,12 @@
 # Created Time: Tue Apr 25 08:10:58 2023
 #########################################################################
 #!/bin/bash
-var=/mnt/var/pp
+var=/mnt/var/pp/
 
 echo "使用tr转换"
 echo $var|tr '/' ' '
 echo "使用sed替换"
 echo $var|sed 's,/, ,g'
 echo "使用gawk解析"
-echo $var|gawk -F / '{print $1  $2  $3}'
+echo $var|gawk -F / '{print $1;print $2;print $3;print $4}'
+echo $var|gawk -F / '{print $1,$2,$3,$4}'
