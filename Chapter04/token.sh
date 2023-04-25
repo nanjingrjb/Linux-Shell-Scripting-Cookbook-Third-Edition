8b8b63049813d0e2b6adb9f7ff1945faeb531a14
nanjingrjb <nanjing@gmail.com>
2023-04-25 11:51:42 +0800

    采用3种方式来解析分隔符，其中sed替换，tr替换结果相同， 而gawk只能得到前2个值，不知何故。

---
+++ Chapter04/token.sh
@@ -0,0 +1,15 @@
+#########################################################################
+# File Name: token.sh
+# Author: ma6174
+# mail: ma6174@163.com
+# Created Time: Tue Apr 25 08:10:58 2023
+#########################################################################
+#!/bin/bash
+var=/mnt/var/pp
+
+echo "使用tr转换"
+echo $var|tr '/' ' '
+echo "使用sed替换"
+echo $var|sed 's,/, ,g'
+echo "使用gawk解析"
+echo $var|gawk -F / '{print $1  $2  $3}'
