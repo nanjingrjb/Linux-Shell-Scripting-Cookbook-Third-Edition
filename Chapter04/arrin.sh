497270a45ca7c68171971298c527a61402711dd2
nanjingrjb <nanjingrjb@gmail.com>
2023-04-28 09:59:50 +0800

    在函数内部传递数组的一种简洁且固定的写法： 1）获取数组固定采用  arr=(`echo $@`) 2)得到数组长度可以采用 len=$# 3)改写得到新数组可以采用    for ((i=0;i<$#;i++))     { 	newarr[$i]=$[$oldarr[$i]*2]     } 4)返回新数组数据为：echo ${newarr[*]} 总之，在函数传递数组时，需要注意一下几点：$@整个数组，$#数组长度，[]改写数组元素值 ()声明数组。(`echo $@`)声明数组。

--- Chapter04/arrin.sh
+++ Chapter04/arrin.sh
@@ -8,17 +8,28 @@
 doublearr()
 {
     cnt=0
-	arr=$@
-	for i in ${arr[*]};
-	do
-		m=$[$i*2]
-	  p[$cnt]=$m
-	  cnt=$[cnt+1]
-	done
+	arr=(`echo $@`)
+#	echo "inside arr is ${arr[*]}"
+#	for i in ${arr[*]};
+#	do
+#		m=$[$i*2]
+#	  p[$cnt]=$m
+#	  cnt=$[cnt+1]
+#	done
+
+#	m= ${#arr[@]}
+	#echo "len is" ${#arr[@]}
+	
+	for ((i=0;i<$#;i++ ))
+	{
+		p[$i]=$[${arr[$i]}*2]
+	}
+
 	echo ${p[*]}
 }
 
 a=(1 2 3 4 5)
+#echo "len arr is :${#a[@]}"
 b=($(doublearr ${a[*]}))
 
 echo "array b is:"${b[*]}
