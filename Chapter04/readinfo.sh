c53d9e24066646da3912fa26d99b08cbe810b767
nanjingrjb <nanjingrjb@gmail.com>
2023-05-10 09:36:33 +0800

    通过source命令简洁读取配置信息，这对于那些非常简单的配置很有效。 实际就是了解source文件的内部解析机制。

---
+++ Chapter04/readinfo.sh
@@ -0,0 +1,12 @@
+#########################################################################
+# File Name: readinfo.sh
+# Author: ma6174
+# mail: ma6174@163.com
+# Created Time: Wed May 10 09:28:39 2023
+#########################################################################
+#!/bin/bash
+source info.txt
+echo "id = $id"
+echo "name = $name"
+echo "phone = $phone"
+
