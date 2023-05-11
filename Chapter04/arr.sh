c4998963058e4d632f0af91420c930df349d9d00
nanjingrjb <nanjing@gmail.com>
2023-05-11 08:00:57 +0800

    数组可以定义为arr=(val val2)     也可以声明后再赋值
        所有元素:${arr[*]}
        元素长度:${#arr[*]}
        元素索引:${!arr[*]}

---
+++ Chapter04/arr.sh
@@ -0,0 +1,20 @@
+#########################################################################
+# File Name: arr.sh
+# Author: ma6174
+# mail: ma6174@163.com
+# Created Time: Thu May 11 07:54:38 2023
+#########################################################################
+#!/bin/bash
+arr=(1 2 3)
+echo ${arr[*]}
+echo ${!arr[*]}
+echo ${#arr[*]}
+declare -A fruits_value
+fruits_value=([apple]='100 dollars' [orange]='150 dollars')
+echo "Apple costs ${fruits_value[apple]}"
+
+
+ declare -A ass_array
+ ass_array=([index1]=val1 [index2]=val2)
+echo ${ass_array[*]}
+ 
