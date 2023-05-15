9cc2724a17a66abb24fd5350473023b879d64709
nanjingrjb <nanjing@gmail.com>
2023-05-16 00:54:13 +0800

    正则表达式提取ip值，其中grep -o用于输出匹配的字符，这个在寻找特定字符串时很有用。

--- Chapter04/ftp.sh
+++ Chapter04/ftp.sh
@@ -10,8 +10,27 @@
 #采用wget命令可以登录ftp服务器，下载对应的文件
 
 des_path=/sdcard/ftp_from_ieslab/src_gcn 
-serv_path=ftp://192.168.3.251:21/diske/SRC_220/01_PRJ/APP_220/src_A_GCN/*
+serv_path=ftp://192.168.3.251:21/diske/SRC_220/01_PRJ/APP_220/src_A_GCN
 wget -r -nH -P $des_path $serv_path	--ftp-user=scusky  --ftp-password=scusky111111 
 
 #将下载后的文件夹删除掉，只保留最终的src_A_GCN文件夹
-echo $des_path
+ip=$(echo $serv_path|grep -o -E "^ftp://.*:21")
+
+dir=${serv_path#"$ip/"}
+
+final_dir="$des_path/$dir"
+echo $ip 
+echo $final_dir.
+des=${des_path%src_gcn}
+echo $des
+if [ -e $des ];
+then rm -fr "$des/*"
+fi
+
+if [ -e $final_dir ];
+then mv $final_dir $des
+else
+	echo "file no exist."
+fi
+#删除原来的文件
+rm -fr $des_path
