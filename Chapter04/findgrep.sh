42c4bd728cbdb6ed279e9e104950a078763c6346
nanjingrjb <nanjing@gmail.com>
2023-05-25 13:38:12 +0800

    在grep中，使用()内用|连接，如寻找两种指定后缀的文件， "\.(txt|sh|conf)"

--- Chapter04/findgrep.sh
+++ Chapter04/findgrep.sh
@@ -5,4 +5,5 @@
 # Created Time: Thu May 25 08:01:44 2023
 #########################################################################
 #!/bin/bash
-find ~/  -type f -name 'test*'|grep -E '(test_st.*)|(testf.*)\.*' |tee findgrep.txt|wc -l
+#find ~/  -type f -name 'test*'|grep -E '(test_st.*)|(testf.*)\.*' |tee findgrep.txt|wc -l
+find ~/  -type f -name 'test*'|grep -E 'test(_st|f|_sp).*\.*(js|conf)' |tee findgrep.txt|wc -l
