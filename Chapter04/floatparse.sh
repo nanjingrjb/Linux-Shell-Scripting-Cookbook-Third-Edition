918cb468b496da9c41b59be1840e1f3ae86cbf5e
nanjingrjb <nanjing@gmail.com>
2023-05-31 23:46:58 +0800

    解析输入数据，判断整数浮点数是否有效。采用了字符串截取操作，分别取整数和小数点部分。

---
+++ Chapter04/floatparse.sh
@@ -0,0 +1,45 @@
+#########################################################################
+# File Name: floatparse.sh
+# Author: ma6174
+# mail: ma6174@163.com
+# Created Time: Wed May 31 23:22:31 2023
+#########################################################################
+#!/bin/bash
+checkint()
+{
+	local or=$(echo $1)
+	local left=$(echo $or|sed 's/[[:digit:]]//g')
+	if [ -z $left ];then
+		echo 0
+	else
+		echo 1
+	fi
+}
+float()
+{
+	in=$(echo $1)
+	int=${in%.*}
+	dec=${in#*.}
+	if [ $dec = $in ];then
+		echo "it seems an int."
+		v1=$(checkint $in)
+		if [ $v1 -eq 0  ] ;
+		then echo "valid int."
+		else
+			echo "wrong int."
+		fi
+	else
+		#echo $int
+		v1=$(checkint $int)
+		#echo $dec
+		v2=$(checkint $dec)
+		if [ $v1 -eq 0 -a $v2 -eq 0 ] ;
+		then echo "valid float."
+		else
+			echo "wrong float"
+		fi
+	fi
+
+}
+
+float $1
