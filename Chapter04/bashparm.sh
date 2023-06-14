47a9c64cb842adbed8b6c594907e550be9229470
nanjingrjb <nanjing@gmail.com>
2023-06-15 07:48:18 +0800

    getopts就是通过移动参数指针来顺序读取。

--- Chapter04/bashparm.sh
+++ Chapter04/bashparm.sh
@@ -13,13 +13,24 @@
 #
 aflag=
 bflag=
-while getopts 'ab:' OPTION
+cflag=
+dflag=
+while getopts 'ab:c:d' OPTION
 do
     case $OPTION in
         a) aflag=1
+echo $OPTIND 
+           ;;
+        d) dflag=1
+echo $OPTIND 
            ;;
         b) bflag=1
+echo $OPTIND 
            bval="$OPTARG"
+           ;;
+        c) cflag=1
+echo $OPTIND 
+           cval="$OPTARG"
            ;;
         ?) printf "Usage: %s: [-a] [-b value] args\n" ${0##*/} >&2
            exit 2
@@ -34,9 +45,17 @@ if [ "$aflag" ]
 then
     printf "Option -a specified\n"
 fi
+
+if [ "$dflag" ]
+then
+    printf "Option -d specified\n"
+fi
 if [ "$bflag" ]
 then
     printf 'Option -b "%s" specified\n' "$bval"
 fi
-printf "Remaining arguments are: %s\n" "$*"
-
+if [ "$cflag" ]
+then
+    printf 'Option -c "%s" specified\n' "$cval"
+fi
+    printf 'Remain parm  "%s" \n' "$*"
