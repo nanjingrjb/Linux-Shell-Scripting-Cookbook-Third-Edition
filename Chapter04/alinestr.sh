0d1c6da8031363cd9221a8298b839c92940b7b6f
nanjingrjb <nanjing@gmail.com>
2023-06-15 18:21:12 +0800

    增加一个读取字符串并分解的函数，其中使用了字符串切片处理: ${str:$i:1}

---
+++ Chapter04/alinestr.sh
@@ -0,0 +1,31 @@
+#########################################################################
+# File Name: alinestr.sh
+# Author: nanjingrjb
+# mail: nanjingrjbgmail.com/g
+# Created Time: Thu Jun 15 18:08:53 2023
+#########################################################################
+#!/bin/bash
+#!/usr/bin/env bash
+# 实例文件：onebyone
+#
+# 从输入中一次解析一个字符
+splitstr()
+{
+	str=$(echo $1)
+	for ((i=0;i<${#str};i++));do  echo ${str:i:1};done
+}
+readstr()
+{
+
+while read ALINE
+do
+    for ((i=0; i < ${#ALINE}; i++))
+    do
+        ACHAR=${ALINE:i:1}
+        # 在这里执行某些操作，如echo $ACHAR
+        echo $ACHAR
+    done
+done
+}
+
+splitstr $*	
