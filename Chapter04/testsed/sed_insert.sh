416b969a4636441ca6d66dfe70f35491a51fb4db
nanjingrjb <nanjing@gmail.com>
2023-07-06 07:35:01 +0800

    插入操作，需要采用sed '1i\string' sed.txt 或sed '1a\string' sed.txt

---
+++ Chapter04/testsed/sed_insert.sh
@@ -0,0 +1,33 @@
+#########################################################################
+# File Name: sed_insert.sh
+# Author: nanjingrjb
+# mail: nanjingrjbgmail.com/g
+# Created Time: Thu Jul  6 07:17:10 2023
+#########################################################################
+#!/bin/bash
+opp=('i' 'a' 'si' 'sa')
+
+echo "原始数据:"
+cat data4.txt
+valid=1
+
+for i in ${opp[*]};
+do
+	case $i in 
+		i) echo "第一行前插入:"
+			res=$(sed '1i\add a line.\n' data4.txt);;
+		a) echo "第一行后插入:"
+			res=$(sed '1a\add a line.\n' data4.txt);;
+		si) echo "搜索test行前插入:"
+			res=$(sed '/test/i\add a line.\n' data4.txt);;
+		sa) echo "搜索test行后插入:"
+			res=$(sed '/test/a\add a line.\n' data4.txt);;
+		*)valid=0;;
+	esac
+	if [ ! $valid ==  0 ]
+	then
+	  echo $res
+	fi
+done
+
+
