83457b295e1ecb470c4f7f72085299d60cc19a1b
nanjingrjb <nanjing@gmail.com>
2023-07-05 12:37:13 +0800

    case本来就作为字符串处理，可以去除引号表示。

--- Chapter04/testsed/sedcase.sh
+++ Chapter04/testsed/sedcase.sh
@@ -12,12 +12,12 @@ valid=1
 for i in ${opp[*]}
 do
 	case $i in
-		"g")echo "全局替换test:"
+		g)echo "全局替换test:"
 			res=$(sed 's/test/trial/g' data4.txt
 			);;
-	    "1")echo "每行第一个替换:" 
+	    1)echo "每行第一个替换:" 
 				res=$(sed 's/test/trial/' data4.txt);;
-		"L1")echo "第一行全替换:" 
+		L1)echo "第一行全替换:" 
 			res=$(sed '1s/test/trial/g' data4.txt);;
 		*)echo "无效选项。"
 			valid=0;; 
