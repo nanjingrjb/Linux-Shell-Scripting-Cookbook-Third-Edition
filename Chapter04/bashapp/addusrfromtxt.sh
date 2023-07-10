812e83b2c1985ee25c57ada7c8d25c908f05eca2
nanjingrjb <nanjing@gmail.com>
2023-07-10 08:13:32 +0800

    增加批量读取用户设置信息，并对这些信息进行分解，采用的方法是: 整行读取，然后采用 $usr=$(echo $line|cut -f1 -d " ")

---
+++ Chapter04/bashapp/addusrfromtxt.sh
@@ -0,0 +1,14 @@
+#########################################################################
+# File Name: addusrfromtxt.sh
+# Author: nanjingrjb
+# mail: nanjingrjbgmail.com/g
+# Created Time: Mon Jul 10 08:03:42 2023
+#########################################################################
+#!/bin/bash
+echo "usage:$0  user.txt"
+while read line
+do #echo $line
+	user=$(echo $line|cut -f1 )
+	pass=$(echo $line|cut -f2 )
+	echo $usr  $pass
+done < $1
