3c7fe381f2d5bf9c6855d516443a449cd79f1d3a
nanjingrjb <nanjing@gmail.com>
2023-07-26 07:10:03 +0800

    sort排序用于处理列表，需要先转化成列表，然后使用sort -nr进行数值排序，或缺省字符排序。
    本例中将字符串转化为列表，然后排序。

--- Chapter04/arrayscan.sh
+++ Chapter04/arrayscan.sh
@@ -26,3 +26,34 @@ for i in ${animals[*]};
 do echo $i;
 done
 
+a[100]=foo
+echo "array len is ${#a[*]}"
+echo "array elem len is ${#a[100]}"
+
+echo "show foo array is:"
+foo=([2]=a [4]=b [6]=c)
+for i in "${foo[*]}";
+do echo $i;
+done
+echo "show foo no empty index is:"
+
+for i in "${!foo[*]}";
+do echo $i;
+done
+
+echo "original arr is:"
+foo=(a b c)
+echo "${foo[@]}"
+foo+=(e f g)
+echo "new len :${#foo[*]}"
+echo "new arr is:"
+for i in  "${foo[@]}";
+do echo $i
+done
+
+
+a=(1 6 56 4 0)
+echo "Original array: ${a[@]}"
+a_sorted=($(for i in "${a[@]}"; do echo $i; done | sort -n))
+echo "Sorted array:   ${a_sorted[@]}"
+
---
+++ Chapter04/sortusing.sh
@@ -0,0 +1,20 @@
+#########################################################################
+# File Name: sortusing.sh
+# Author: nanjingrjb
+# mail: nanjingrjbgmail.com/g
+# Created Time: Wed Jul 26 07:00:56 2023
+#########################################################################
+#!/bin/bash
+echo "sort 用于处理列表输入"
+
+seq 1 9|sort -nr
+
+#将字符串转为列表
+changestr2lst()
+{
+	for i in $1;
+	do echo $i
+	done
+}
+
+changestr2lst "1 3 -56 20 36 87"|sort -nr
