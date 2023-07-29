791e0fa18de292c08479f1c48e0a1e109304ad3f
nanjingrjb <nanjing@gmail.com>
2023-07-29 09:28:24 +0800

    寻找某一文件夹下指定关键字的文件 bash findfile path type key

---
+++ Chapter04/findfile.sh
@@ -0,0 +1,38 @@
+#########################################################################
+# File Name: findfile.sh
+# Author: nanjingrjb
+# mail: nanjingrjbgmail.com/g
+# Created Time: Sat Jul 29 08:36:16 2023
+#########################################################################
+#!/bin/bash
+#
+wcfile()
+{
+	if [ -z $1 ]
+	then
+        echo "usage:$0 path type key,type&&key no -link."
+		exit
+		dir=./
+	else
+	    dir=$1
+	fi
+
+	if [ -z $2 ]
+	then
+		type="f"
+	else
+	    type="$2"
+	fi
+	
+	if [ -z $3 ] 
+	then
+		key="-name file*"
+	else
+	    key="$3"
+	fi
+
+	find $dir  -type $type -name "*.${key}" 
+
+}
+wcfile $*
+
