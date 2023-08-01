56aa4636e1e821b4d4458b7c208e7b9bc7be1f99
nanjingrjb <nanjing@gmail.com>
2023-08-02 07:55:03 +0800

    定义字符的寻找函数，采用正则匹配方法。

---
+++ Chapter04/conditionjudge.sh
@@ -0,0 +1,17 @@
+#########################################################################
+# File Name: conditionjudge.sh
+# Author: nanjingrjb
+# mail: nanjingrjbgmail.com/g
+# Created Time: Wed Aug  2 07:39:38 2023
+#########################################################################
+#!/bin/bash
+findstr()
+{
+	str="$1"
+	fstr="$2"
+	echo "$str"|grep -E "$fstr" &&echo "match" ||echo "no find $fstr in $str"
+
+}
+
+findstr "$@"
+
