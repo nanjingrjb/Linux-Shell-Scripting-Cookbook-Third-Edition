4acd353008bf1a7300fc2799b33f5441a6090a8a
nanjingrjb <nanjingrjb@gmail.com>
2023-08-01 08:35:25 +0800

    排除法通常用中括号来表示，如不含ass或go，表示为[^(ass|go)] 含有特定的字符组合，用小括号表示，如以tea或sea开头，^(tea|sea) 以ful或ing结尾的单词，如(ful|ing)$
    完整正则表达式为：
    grep "^(tea|sea)[^(ass|go)].*(ful|ing)$" /usr/share/dict/words

--- Chapter04/dict_reg.sh
+++ Chapter04/dict_reg.sh
@@ -5,4 +5,5 @@
 # Created Time: Tue Aug  1 08:12:33 2023
 #########################################################################
 #!/bin/bash
-grep -E "(^sea|^tea).*[^('s)|(ful)|(zle)|(oom)]$" /usr/share/dict/words |grep -E "(ing|ser)$"|wc -l
+#以sea或tea结尾，中间不含有ass或go，以ful或ing结尾的单词
+grep -E "^(sea|tea)[^(ass|go)].*(ful|ing)$" /usr/share/dict/words 
