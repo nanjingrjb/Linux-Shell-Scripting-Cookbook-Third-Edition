c25d0f4c35c37da55223b610d1fce2345973e3c1
nanjingrjb <nanjingrjb@gmail.com>
2023-08-10 14:48:24 +0800

    增加ubuntu容器的测试 其中docker container ls列出当前的容器 docker image pull ubuntu:latest  从docker主机上获取最新的ubuntu镜像 docker image ls 列出当前所有镜像 docker container run -it ubuntu:latest /bin/bash          启动ubuntu容器的镜像中的bash程序 docker container ls  查看运行的容器信息，通常为： CONTAINER ID   IMAGE           COMMAND       CREATED         STATUS         PORTS     NAMES dfdf3fa50d24   ubuntu:latest   "/bin/bash"   2 minutes ago   Up 2 minutes             vigilant_pare
    其中最重要的是NAMES信息，如果要远方登录此容器，可以使用如下命令：
    docker  container exec -it vigilant_pare bash

---
+++ Chapter04/linkubu.sh
@@ -0,0 +1,3 @@
+#登录ubuntu容器，并可以执行了
+
+docker container exec -it vigilant_pare bash 
---
+++ Chapter04/wget.sh
@@ -0,0 +1,8 @@
+#########################################################################
+# File Name: wget.sh
+# Author: ma6174
+# mail: ma6174@163.com
+# Created Time: Mon May 15 15:20:13 2023
+#########################################################################
+#!/bin/bash
+wget -r -nH -P/webapp/ ftp://172.31.1.1:21/* --ftp-user=ftpuser --ftp-password=ftpusewget -r -nH -P/webapp/ ftp://172.31.1.1:21/* --ftp-user=ftpuser --ftp-password=ftpuserr
