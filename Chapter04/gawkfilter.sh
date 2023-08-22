dc5ab68a694ea5af4d1d0054c89b2823c208c40c
root <root@localhost.localdomain>
2023-08-22 23:46:43 +0000

    对比可知对于结构化数据，用gawk指定特性可以精准找到对应数据，而grep只是单纯字符查找，对应不了实际对应的数据。

---
+++ Chapter04/gawkfilter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# ***************************************************************************
+# * 
+# * @file:gawkfilter.sh 
+# * @author:nanjingrjb@gmail.com 
+# * @date:2023-08-22 23:42 
+# * @version 1.0  
+# * @description: Shell script 
+# * @Copyright (c)  all right reserved 
+#* 
+#**************************************************************************/ 
+
+
+
+echo "gawk匹配对应的项目"
+
+cat /etc/passwd|gawk -F: '$4 ~/104/ {print $0}'
+
+echo "grep输出:"
+grep -E '104' /etc/passwd
+
---
+++ Chapter04/subtoken.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ************************************************************************ 
+# * 
+# * @file:subtoken.py 
+# * @author:nanjingrjb@gmail.com 
+# * @date:2023-08-22 12:05 
+# * @version 1.0  
+# * @description: Python Script 
+# * @Copyright (c)  all right reserved 
+# * 
+#************************************************************************* 
+
+import os,sys
+def subtoken():
+    {
+            for i in range(1,3):
+            print(sys.argv[i])
+    }
+
+subtoken() 
