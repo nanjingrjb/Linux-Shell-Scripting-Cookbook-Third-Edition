3d263b141047f57781247593c960c1714a5872d5
nanjingrjb <nanjingrjb@gmail.com>
2023-09-14 16:21:47 +0800

    just for a test.

---
+++ Chapter04/myrsync_svn.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ***************************************************************************
+# * 
+# * @file:rsync_svn.sh 
+# * @author:nanjingrjb@gmail.com 
+# * @date:2023-09-13 10:30 
+# * @version 1.0  
+# * @description: Shell script 
+# * @Copyright (c)  all right reserved 
+#* 
+#**************************************************************************/ 
+
+
+
+rsync -avt /mnt/e/Repositories/  /mnt/f/全部仓库文件/Repositories/
+
