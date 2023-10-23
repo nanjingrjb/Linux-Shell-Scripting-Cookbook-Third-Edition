e2d21c62473bbf25233fd2afc0a614e5c2c07b1f
nanjingrjb <nanjingrjb@gmail.com>
2023-10-23 20:31:59 +0800

    采用sed调用sed程序文件，其中支持扩展正则表达式， sed -Eif sedfun.sh sed.txt i表示实时写入。

---
+++ Chapter04/sed_func.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ***************************************************************************
+# * 
+# * @file:sed_func.sh 
+# * @author:nanjingrjb@gmail.com 
+# * @date:2023-10-23 20:30 
+# * @version 1.0  
+# * @description: Shell script 
+# * @Copyright (c)  all right reserved 
+#* 
+#**************************************************************************/ 
+
+
+
+sed  -Ei -f  sedfunc.sh sed.txt
+
---
+++ Chapter04/sedfunc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ***************************************************************************
+# * 
+# * @file:sedfunc.sh 
+# * @author:nanjingrjb@gmail.com 
+# * @date:2023-10-23 18:01 
+# * @version 1.0  
+# * @description: Shell script 
+# * @Copyright (c)  all right reserved 
+#* 
+#**************************************************************************/ 
+
+
+
+s/end/start/g
+s/[0-9]{1,}/x/g
+/^[[:space:]]*$/d
