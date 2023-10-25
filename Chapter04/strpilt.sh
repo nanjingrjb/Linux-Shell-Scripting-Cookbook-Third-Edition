4ea11092c3bed1111de7ba447643d1ab3f85eea4
nanjingrjb <nanjingrjb@gmail.com>
2023-10-26 07:26:04 +0800

    截取字符串 百子星左 井星子右

---
+++ Chapter04/strpilt.sh
@@ -0,0 +1,25 @@
+i#!/bin/bash
+
+# ***************************************************************************
+# * 
+# * @file:strpilt.sh 
+# * @author:nanjingrjb@gmail.com 
+# * @date:2023-10-26 07:19 
+# * @version 1.0  
+# * @description: Shell script 
+# * @Copyright (c)  all right reserved 
+#* 
+#**************************************************************************/ 
+v=123.456.789
+s1=${v#*.}
+s2=${v%.*}
+echo "s1=$s1"
+echo "s2=$s2"
+
+s3=${v##*.}
+s4=${v%%.*}
+echo "s3=$s3"
+echo "s4=$s4"
+
+
+
