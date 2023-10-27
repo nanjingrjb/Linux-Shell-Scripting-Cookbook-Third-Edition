d12696ce3a84be2668388e36da800c14b82763a6
nanjingrjb <nanjingrjb@gmail.com>
2023-10-27 13:48:27 +0800

    对PATH环境变量进行检查，对于其中的无效路径进行判断。 通过改写PATH中的无效路径来实现。
    其中grep只能是显示对应行的信息，而目前PATH输出文件实际是一行，
    故并不能直接grep -v $misspath .
    
    而应该采用sed 来删除或替换，而进行删除时，是删除对应的一行，故不可行。
    所以只能进行替换，将其中的无效路径替换为空格，也能达到删除的效果。
    
    而路径是以/开头的，sed命令也是以/开头，这样会引起混淆，所以采用
    sed替代符号#来区分。
    
    又由于sed要支持变量，所以对sed命令采用双引号""括起来，整个代码结构如下
    first=1
    pathfile=$PATH
    while read misspath
        do
    	if [  ! -d "$misspath" ];then
    	    if [ $first -eq 1 ];then
    	    echo "$pathfile"|sed "s#$misspath##g" >tmp_path.txt
    	    else
    	    cat "$pathfile"|sed "s#$misspath##g" >tmp_path.txt
    	    fi
    	    pathfile=tmp_file.txt
    	 fi
        done<alll_file.txt

---
+++ Chapter04/checkPathValid.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# ***************************************************************************
+# * 
+# * @file:checkPathValid.sh 
+# * @author:nanjingrjb@gmail.com 
+# * @date:2023-10-27 10:10 
+# * @version 1.0  
+# * @description: Shell script 
+# * @Copyright (c)  all right reserved 
+#* 
+#**************************************************************************/ 
+
+
+
+echo $PATH|gawk -F: '{for(i=1;i<NF;i++)
+						print $i}'  >'pathdir.txt'
+
+
+
+mypath=$PATH
+first=1
+while read elm
+do
+	if [ ! -d "$elm" ];then
+		echo "dir $elm no exist."
+		if [ $first -eq 1 ];then
+			first=0
+			echo $PATH|sed   "s#$elm##g" >tmp_path.txt
+		else
+		    cat "$mypath"|sed  "s#$elm##g" >tmp_path.txt
+		fi
+		mypath=tmp_path.txt
+	fi
+done<'pathdir.txt'
+
