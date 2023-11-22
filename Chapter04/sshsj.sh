4d2002f290041a74eaf3d954e8fe84f6df61cc7c
nanjingrjb <nanjingrjb@gmail.com>
2023-11-22 10:29:28 +0800

    对于sshsj进行公司识别，目前不再将公司电脑固定，而直接ping 网关地址 192.168.3.1，只要能ping通，则说明为公司网址，所以 可以直接连接wlan中的手机地址。 目前小米k60手机的连接地址为192.168.3.16

--- Chapter04/sshsj.sh
+++ Chapter04/sshsj.sh
@@ -5,13 +5,13 @@
 # Created Time: Mon Jul 31 13:13:02 2023
 #########################################################################
 #!/bin/bash
-ip_jcrj=192.168.3.251
+ip_jcrj=192.168.3.1
 ip_home=192.168.1.1
 
 ping  -c 2 -i 0.2 -W 1 "$ip_jcrj" &> /dev/null
 if [ $? -eq 0 ]
 then
-	sshpass -p "123456" ssh -p 8022 root@192.168.3.206  
+	sshpass -p "123456" ssh -p 8022 root@192.168.3.16  
 	#密码为123456
 else
         ping  -c 2 -i 0.2 -W 1 "$ip_home" &> /dev/null
