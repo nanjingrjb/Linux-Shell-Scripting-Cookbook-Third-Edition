200bcc386fa40b15c83f769b4445e46cfc95fdcc
nanjingrjb <nanjingrjb@gmail.com>
2024-03-23 08:07:15 +0800

    ok

---
+++ Chapter04/pyver.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ************************************************************************ 
+# * 
+# * @file:pyver.py 
+# * @author:nanjingrjb@gmail.com 
+# * @date:2023-12-31 13:27 
+# * @version 1.0  
+# * @description: Python Script 
+# * @Copyright (c)  all right reserved 
+# * 
+#************************************************************************* 
+
+import os,sys
+print(sys.version)
+
