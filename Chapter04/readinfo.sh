8ca2cbace03ce39bb9ff332c7c2fd296987c47e9
nanjingrjb <nanjingrjb@gmail.com>
2024-11-28 08:13:42 +0800

    read读取多个变量，

--- Chapter04/readinfo.sh
+++ Chapter04/readinfo.sh
@@ -1,11 +1,20 @@
-#########################################################################
-# File Name: readinfo.sh
-# Author: ma6174
-# mail: ma6174@163.com
-# Created Time: Wed May 10 09:28:39 2023
-#########################################################################
 #!/bin/bash
-for i in $(cat info.txt);do echo " $i";done
+
+# ***************************************************************************
+# * 
+# * @file:readinfo.sh 
+# * @author:nanjingrjb@gmail.com 
+# * @date:2024-11-28 08:09 
+# * @version 1.0  
+# * @description: Shell script 
+# * @Copyright (c)  all right reserved 
+#* 
+#**************************************************************************/ 
 
 
-gawk -d '{print "index="$1,"name=" $2;}' info.txt
+
+
+while read name age addr
+do
+	echo "$name,$age,$addr"
+done <info
